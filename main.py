13385402ae6fc0984dd6a51a335f966d9fed32a6
RanaAqib <55365820+RanaAqib@users.noreply.github.com>
2019-09-16 06:19:29 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+r=3
+area=3.14*r**2
+print('Area of Cicle is: '+str(area))
+
---
+++ second.py
@@ -0,0 +1,5 @@
+first_name='Rana'
+last_name='Momin'
+age=20
+study=12
+print(first_name+' '+last_name+' is the student of class '+str(study)+'. His age is '+str(age))
